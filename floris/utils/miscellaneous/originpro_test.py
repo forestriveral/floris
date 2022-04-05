d8b7a9d07f4a574f25e745a64cfac080689379ef
Hang Li <forestriveral@gmail.com>
2022-04-05 20:37:06 +0800

    Latex installing and coding debug

--- floris/utils/miscellaneous/originpro_test.py
+++ floris/utils/miscellaneous/originpro_test.py
@@ -46,10 +46,62 @@ def data_reader_test():
     # exp_xy = [~np.isnan(a).any(axis=1)]
     exp_xy = ori_data[['exp_x', 'exp_y']].dropna(axis=0, how='all').values
     les1_xy = ori_data[['les1_x', 'les1_y']].dropna(axis=0, how='all').values
+    print(np.all(~np.isnan(exp_xy)))
+    assert np.all(~np.isnan(exp_xy)) and np.all(~np.isnan(les1_xy))
     print(exp_xy.shape)
     print(les1_xy.shape)
+
+
+def sample_test(id=0):
+    # Ensures that the Origin instance gets shut down properly.
+    import sys
+    def origin_shutdown_exception_hook(exctype, value, traceback):
+        op.exit()
+        sys.__excepthook__(exctype, value, traceback)
+    if op and op.oext:
+        sys.excepthook = origin_shutdown_exception_hook
+
+
+    # Set Origin instance visibility.
+    if op.oext:
+        op.set_show(True)
+
+    # YOUR CODE HERE
+    if id == 0:
+
+        # Example of opening a project and reading data.
+        # We'll open the Tutorial Data.opju project that ships with Origin.
+        src_opju = op.path('e') + r'Samples\Tutorial Data.opju'
+        op.open(file = src_opju, readonly = True)
+        # Simple syntax to find a worksheet.
+        src_wks = op.find_sheet('w', 'Book1A')
+        # Pull first column data into a list and dump to screen.
+        lst = src_wks.to_list(0)
+        print(*lst, sep = ", ")
+        # Pull entire worksheet into a pandas DataFrame and partially dump to screen.
+        # Column long name will be columns name.
+        df = src_wks.to_df()
+        print(df.head())
+        # Start a new project which closes the previous one.
+        op.new()
+        #Examples of writing data to a project and saving it.
+        # We'll reuse the data objects we previously created.
+        # Simple syntax to create a new workbook with one sheet
+        dest_wks = op.new_sheet('w')
+        # Insert list data into columns 1
+        dest_wks.from_list(0, lst)
+        # Add another sheet top above workbook and add the DataFrame data.
+        # DataFrame column names with be Origin column Long Names.
+        dest_wks.get_book().add_sheet().from_df(df)
+        # Save the opju to your UFF.
+        op.save(op.path('u')+ 'Ext Python Example 1.opju')
+
+    # Exit running instance of Origin.
+    if op.oext:
+        op.exit()
 
 
 if __name__ == "__main__":
     # simple_draw()
     data_reader_test()
+    # sample_test(id=0)
